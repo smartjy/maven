0dcd6fdff4ae2e24fc796f4dac8f7b9c559d5dba
jyson <jyson@osci.kr>
2020-01-31 01:03:02 +0900

    Initial commit

---
+++ mvn-firstapp/src/main/java/com/jyson/App.java
@@ -0,0 +1,14 @@
+package com.jyson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "my name is ... " );
+		System.out.println( "maven work" );		
+    }
+}
